ac8bb627af9d90f38c8dc86f94743eee12b86040
Behzad-Az <behy1368@gmail.com>
2019-05-23 14:24:35 +0000

    set up bulma card building blocks

--- client/source/js/components/global/Menu.jsx
+++ client/source/js/components/global/Menu.jsx
@@ -6,47 +6,80 @@ import workAndCoLogoImg from 'img/workco-logo.svg';
 export default class Menu extends Component {
   render() {
     return (
-      <div className='Menu'>
-        <div className='Menu-logo'>
-          <a href='https://work.co' target='_blank' rel='noreferrer noopener' aria-label='Work & Co website'>
-            <img
-              src={ workAndCoLogoImg }
-              alt='Work & Co logo'
-            />
+
+      <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='https://bulma.io'>
+            <img src='https://bulma.io/images/bulma-logo.png' width='112' height='28' />
+          </a>
+
+          <a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
           </a>
         </div>
-        <div className='Menu-links'>
-          <NavLink
-            activeClassName='Menu-link--active'
-            className='Menu-link'
-            exact
-            to={ routeCodes.HOME }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            activeClassName='Menu-link--active'
-            className='Menu-link'
-            to={ routeCodes.PEOPLE }
-          >
-            API data example
-          </NavLink>
-          <NavLink
-            activeClassName='Menu-link--active'
-            className='Menu-link'
-            to={ routeCodes.CHARTS }
-          >
-            Charts
-          </NavLink>
-          <NavLink
-            activeClassName='Menu-link--active'
-            className='Menu-link'
-            to='/404'
-          >
-            404
-          </NavLink>
+
+        <div id='navbarBasicExample' className='navbar-menu'>
+          <div className='navbar-start'>
+
+            <NavLink
+              activeClassName='Menu-link--active'
+              className='Menu-link navbar-item'
+              exact
+              to={ routeCodes.HOME }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              activeClassName='Menu-link--active'
+              className='Menu-link navbar-item'
+              to={ routeCodes.PEOPLE }
+            >
+              API data example
+            </NavLink>
+            <NavLink
+              activeClassName='Menu-link--active'
+              className='Menu-link navbar-item'
+              to={ routeCodes.CHARTS }
+            >
+              Charts
+            </NavLink>
+            <NavLink
+              activeClassName='Menu-link--active navbar-item is-active'
+              className='Menu-link navbar-item'
+              to={ routeCodes.LANDING }
+            >
+              Landing
+            </NavLink>
+            <NavLink
+              activeClassName='Menu-link--active navbar-item is-active'
+              className='Menu-link navbar-item'
+              to='/404'
+            >
+              404
+            </NavLink>
+
+
+          </div>
+
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <div className='buttons'>
+                <a className='button is-primary'>
+                  <strong>Sign up</strong>
+                </a>
+                <a className='button is-light'>
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
+
+
+
     );
   }
 }
--- client/source/js/views/Landing.jsx
+++ client/source/js/views/Landing.jsx
@@ -40,78 +40,98 @@ export default class Landing extends Component {
     // } = this.props;
 
     return (
-      <div className='landing-page columns'>
-
-        <div className='column is-one-third'>
-          <div className='list is-hoverable'>
-            <a className='list-item'>
-              Featured
-            </a>
-            <a className='list-item'>
-              All Posts
-            </a>
-            <a className='list-item is-active'>
-              Announcements
-            </a>
-            <a className='list-item'>
-              Business
-            </a>
-            <a className='list-item'>
-              Community
-            </a>
-            <a className='list-item'>
-              Data
-            </a>
-            <a className='list-item'>
-              Editor Tools
-            </a>
-            <a className='list-item'>
-              Education
-            </a>
-            <a className='list-item'>
-              Engineering
-            </a>
-            <a className='list-item'>
-              Events
-            </a>
-            <a className='list-item'>
-              Policy
-            </a>
-          </div>
-        </div>
-
-        <div className='column is-two-thirds'>
-          <div className='card'>
-            <div className='card-image'>
-              <figure className='image is-4by3'>
-                <img src='https://bulma.io/images/placeholders/1280x960.png' alt='Placeholder image' />
-              </figure>
-            </div>
-            <div className='card-content'>
-              <div className='media'>
-                <div className='media-left'>
-                  <figure className='image is-48x48'>
-                    <img src='https://bulma.io/images/placeholders/96x96.png' alt='Placeholder image' />
-                  </figure>
-                </div>
-                <div className='media-content'>
-                  <p className='title is-4'>John Smith</p>
-                  <p className='subtitle is-6'>@johnsmith</p>
-                </div>
-              </div>
-
-              <div className='content'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href='#'>#css</a> <a href='#'>#responsive</a>
-                <br />
-                <time dateTime='2016-1-1'>11:09 PM - 1 Jan 2016</time>
-              </div>
+      <div className='landing-page container'>
+        <div className='columns'>
+          <div className='column is-one-third'>
+            <div className='list is-hoverable'>
+              <a className='list-item'>
+                Featured
+              </a>
+              <a className='list-item'>
+                All Posts
+              </a>
+              <a className='list-item is-active'>
+                Announcements
+              </a>
+              <a className='list-item'>
+                Business
+              </a>
+              <a className='list-item'>
+                Community
+              </a>
+              <a className='list-item'>
+                Data
+              </a>
+              <a className='list-item'>
+                Editor Tools
+              </a>
+              <a className='list-item'>
+                Education
+              </a>
+              <a className='list-item'>
+                Engineering
+              </a>
+              <a className='list-item'>
+                Events
+              </a>
+              <a className='list-item'>
+                Policy
+              </a>
             </div>
           </div>
+
+          <div className='column'>
+            <div className='card'>
+              <div className='card-image'>
+                <figure className='image is-3by2 is-marginless'>
+                  <img src='https://bulma.io/images/placeholders/480x320.png' alt='Placeholder image' />
+                </figure>
+              </div>
+              <div className='card-content'>
+
+
+                <article className='media'>
+                  <div className='media-content'>
+                    <div className='content'>
+                      <p>
+                        <strong>Kayli Eunice </strong>
+                        <br />
+                        Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
+                        <br />
+                        <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+                      </p>
+                    </div>
+                  </div>
+                </article>
+
+                <article className='media'>
+                  <div className='media-content'>
+                    <div className='content'>
+                      <p>
+                        <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+
+                <article className='media'>
+                  <div className='media-content'>
+                    <div className='content'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                      <a href='#'>#css</a> <a href='#'>#responsive</a>
+                      <br />
+                      <time dateTime='2016-1-1'>11:09 PM - 1 Jan 2016</time>
+                    </div>
+                  </div>
+                </article>
+
+              </div>
+            </div>
+          </div>
         </div>
-
-
       </div>
     );
   }
