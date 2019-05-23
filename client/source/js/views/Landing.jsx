71292f54255e49f401d1e123d9f8a61166ba3efa
Behzad-Az <behy1368@gmail.com>
2019-05-23 15:19:12 +0000

    added search box component to landing page

--- client/source/js/views/Landing.jsx
+++ client/source/js/views/Landing.jsx
@@ -41,8 +41,24 @@ export default class Landing extends Component {
 
     return (
       <div className='landing-page container'>
+        <br />
         <div className='columns'>
           <div className='column is-one-third'>
+
+            <div className='field has-addons'>
+              <div className='control has-icons-left is-expanded'>
+                <input className='input' type='text' placeholder='Find a part #' />
+                <span className='icon is-left'>
+                  <i className='fas fa-search'></i>
+                </span>
+              </div>
+              <div className='control'>
+                <a className='button is-info'>
+                  Search
+                </a>
+              </div>
+            </div>
+
             <div className='list is-hoverable'>
               <a className='list-item'>
                 Featured
